f2f72a42703598b0fceeed5a3ca3490d8b73b857
Kristina <kristina.hodges7@gmail.com>
2022-08-29 11:28:48 -0400

    commit tree 23f9e2dc123cee3f189772210ebac737ef91d630

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+require 'pry'
+def badge_maker name 
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator namesArr
+  namesArr.map do |name|
+  "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms list_of_speakers
+  rooms = []
+  new_list = list_of_speakers.each_with_index do |speaker, index|
+    rooms << "Hello, #{speaker}! You'll be assigned to room #{index += 1}!"
+  end
+  rooms
+end
+
+def printer (namesArr)
+  batch_badge_creator(namesArr).each do |speaker|
+    puts speaker
+  end
+  assign_rooms(namesArr).each do |message|
+    puts message
+  end
+end
